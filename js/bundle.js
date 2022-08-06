9f473b56206232a707139238c1a06aeb393528f0
Mission Bitcoin <99844176+mission-bitcoin@users.noreply.github.com>
2022-08-07 05:53:16 +0930

    Add files via upload

--- js/bundle.js
+++ js/bundle.js
@@ -58488,6 +58488,11 @@ button.addEventListener("mousedown", pubKeytoReceivingAddr);
 buttonKeyPair.addEventListener("mousedown", generateRandomKeyPair);
 
 function pubKeytoReceivingAddr() {
+  const mempoolSpaceURL = document.getElementById("mempoolSpaceURL");
+  const btcBalance = document.getElementById("btcBalance");
+  const btcReceived = document.getElementById("btcReceived");
+  const btcSpent = document.getElementById("btcSpent");
+
   let address = {},
     inputPubKey = document.getElementById("publicKey").value,
     pubkey = Buffer.from(inputPubKey, "hex"),
@@ -58513,14 +58518,21 @@ function pubKeytoReceivingAddr() {
         pubkey,
       });
     }
-    receivingAddressOutput.value = `${address.address}`;
+
+    receivingAddressOutput.value = address.address;
+    mempoolSpaceURL.innerHTML = `<a href="https://mempool.space/address/${address.address}" id="mempoolSpaceURL"><font color="orange">Mempool.space Stats<br>(view more detail)</font></a>`;
+    getAddressInfo(address.address);
   } else {
     receivingAddressOutput.value = "";
+    mempoolSpaceURL.innerHTML = `<a href="https://mempool.space" id="mempoolSpaceURL"><font color="orange"><b>Mempool.space Stats</b></font></a>`;
+    btcBalance.innerHTML = "N/A";
+    btcReceived.innerHTML = "N/A";
+    btcSpent.innerHTML = "N/A";
   }
 }
 
 function generateRandomKeyPair() {
-  let randomPrivateKey = new bitcorejs.PrivateKey(), // Can pass in WIF PrivKey if preferred over random, eg 'KySeaS5oD47g5Egw1PjrZJW63DhvoXpxipcyifiT9T1qmuGbiVz1'
+  let randomPrivateKey = new bitcorejs.PrivateKey(),
     randomPublicKey = randomPrivateKey.toPublicKey(),
     randomPrivateKeyWIF = randomPrivateKey.toWIF(),
     randomReceivingAddress = randomPrivateKey.toAddress();
@@ -58529,6 +58541,22 @@ function generateRandomKeyPair() {
     (document.getElementById("randomPrivateKey").value = randomPrivateKeyWIF),
     (document.getElementById("randomReceivingAddress").value =
       randomReceivingAddress);
+}
+
+function getAddressInfo(address) {
+  fetch("https://mempool.space/api/address/" + address)
+    .then((response) => response.json())
+    .then((data) => {
+      let currentBalance =
+        data.chain_stats.funded_txo_sum / 100000000 -
+        data.chain_stats.spent_txo_sum / 100000000;
+      let pastReceived = data.chain_stats.funded_txo_sum / 100000000;
+      let pastSpent = data.chain_stats.spent_txo_sum / 100000000;
+
+      btcBalance.innerHTML = currentBalance;
+      btcReceived.innerHTML = pastReceived;
+      btcSpent.innerHTML = pastSpent;
+    });
 }
 
 }).call(this)}).call(this,require("buffer").Buffer)
